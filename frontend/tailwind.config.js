efcb089c63d868f795461db7ce80f761ae08d1de
VictoriaPugach <pugachviktoria@yandex.ru>
2025-11-27 23:49:17 +0300

    initial front + auth

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        "variable-collection-stroke": "var(--variable-collection-stroke)",
+      },
+      animation: {
+        'gradient': 'gradient 8s ease infinite',
+      },
+      keyframes: {
+        gradient: {
+          '0%, 100%': {
+            'background-position': '0% 50%'
+          },
+          '50%': {
+            'background-position': '100% 50%'
+          },
+        }
+      }
+    },
+  },
+  plugins: [],
+};
+
