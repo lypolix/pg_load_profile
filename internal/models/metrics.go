08f2eb37211cb0b5996c557943a82c3bf2f37406
lypolix <m2414154@edu.misis>
2025-11-27 19:15:15 +0300

    feat(metrics) added new metics

--- internal/models/metrics.go
+++ internal/models/metrics.go
@@ -2,14 +2,24 @@ package models
 
 // WorkloadMetrics содержит рассчитанные метрики профиля
 type WorkloadMetrics struct {
-	DBTimeTotal     float64 // Общее время базы (из pg_stat_statements)
-	DBTimeCommitted float64 // Чистое время работы (DB Time - Waits)
-	CPUTime         float64 // Время на CPU
-	IOTime          float64 // Время на I/O
-	LockTime        float64 // Время на блокировках
+	// --- DB Time Metrics (Уже были) ---
+	DBTimeTotal     float64 `json:"db_time_total"`
+	DBTimeCommitted float64 `json:"db_time_committed"`
+	CPUTime         float64 `json:"cpu_time"`
+	IOTime          float64 `json:"io_time"`
+	LockTime        float64 `json:"lock_time"`
+
+	CPUPercent  float64 `json:"cpu_percent"`
+	IOPercent   float64 `json:"io_percent"`
+	LockPercent float64 `json:"lock_percent"`
+
+	// --- NEW: Throughput & Counters (Новые) ---
+	TPS              float64 `json:"tps"`                 // Транзакций в секунду
+	QPS              float64 `json:"qps"`                 // Запросов в секунду
+	AvgLatency       float64 `json:"avg_query_latency_ms"`// Среднее время запроса
+	RollbackRate     float64 `json:"rollback_rate"`       // Откатов в секунду
 	
-	// Процентные соотношения для удобства классификации
-	CPUPercent  float64
-	IOPercent   float64
-	LockPercent float64
+	TotalCommits     int64   `json:"total_commits"`       // Счетчик на момент замера
+	TotalRollbacks   int64   `json:"total_rollbacks"`     // Счетчик на момент замера
+	TotalCalls       int64   `json:"total_calls"`         // Счетчик pg_stat_statements
 }
